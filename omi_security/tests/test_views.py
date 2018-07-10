f5b4958bb920f5317682de03318fd5eef7912a0d
Aisha Saeed <aisha.saeed@aalto.fi>
2018-07-11 01:13:52 +0300

    Added admin panel to add/remove superusers. Removed superuser option from sign up page.

--- omi_security/tests/test_views.py
+++ omi_security/tests/test_views.py
@@ -17,8 +17,17 @@ class LogInTest(TestCase):
                 'email': 'foobar@gmail.com',
                 'password': 'foobar',
                 'password1': 'foobar',
+                }
+
+    def form_params_admin_panel(self):
+        return {'first_name': 'hellobar',
+                'last_name': 'hellobar',
+                'username': 'hellobar',
+                'email': 'hellobar@gmail.com',
+                'password': 'hellobar',
+                'password1': 'hellobar',
                 'is_superuser': True,
-                'superuser_secret': 'Iamsuperuser12345',}
+                }
 
     def test_login(self):
         user_login = self.client.login(username="testuser", password="secret")
@@ -66,3 +75,38 @@ class LogInTest(TestCase):
         #you = User.objects.filter(username=expected_username)
         #for y in you:
             #print(y.__dict__)
+    def test_admin_panel(self):
+        password = 'adminpassword'
+        my_admin = User.objects.create_superuser('adminuser', 'adminemail@test.com', password)
+        self.client.login(username=my_admin.username, password=password)
+        response = self.client.get("/create_oauth_token")
+        self.assertEqual(response.status_code, 302)
+        u = User.objects.get(username='adminuser')
+        self.assertTrue(u.is_superuser)
+        response = self.client.get("/superusers_panel")
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_panel_normal_user(self):
+        self.client.login(username='testuser', password='secret')
+        response = self.client.get("/create_oauth_token")
+        self.assertEqual(response.status_code, 302)
+        u = User.objects.get(username='testuser')
+        self.assertFalse(u.is_superuser)
+        response = self.client.get("/superusers_panel")
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_admin_panel(self):
+        params = self.form_params_admin_panel()
+        expected_username = params['username']
+
+        password = 'adminpassword'
+        my_admin = User.objects.create_superuser('adminuser', 'adminemail@test.com', password)
+        self.client.login(username=my_admin.username, password=password)
+        self.client.get("/create_oauth_token")
+        self.client.post("/superusers_panel", params)
+
+        self.assertTrue(User.objects.filter(username=expected_username).exists(),
+                "SuperUser was not created.")
+
+        new_user = User.objects.get(username=expected_username)
+        self.assertTrue(new_user.is_superuser)
